fab43300c5ed7bc426c0027b2a3d4e9f9c8b980a
Lucas Ferrari <lucasferrarisoares@gmail.com>
2026-03-13 11:12:19 -0300

    add project base

---
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -0,0 +1,17 @@
+import IRestaurante from '../../interfaces/IRestaurante';
+import style from './ListaRestaurantes.module.scss';
+import Restaurante from './Restaurante';
+
+const ListaRestaurantes = () => {
+
+  const restaurantes: IRestaurante[] = [
+    
+  ]
+
+  return (<section className={style.ListaRestaurantes}>
+    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+  </section>)
+}
+
+export default ListaRestaurantes
